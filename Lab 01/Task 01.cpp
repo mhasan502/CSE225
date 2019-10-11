c0ba5a250eb4b8ac6229a647167fbfb9b72c7a67
mhasan502 <mhasan502@gmail.com>
2019-10-11 23:50:00 +0600

    Create Task 01.cpp

---
+++ Lab 01/Task 01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arraySize;    //Array Size initialize
+    cin >> arraySize;    //Input Array Size
+    
+    int *ptr = new int[arraySize];    //Allocate an int array
+
+    for(int i=0; i<arraySize; i++){    //Assigning values
+        cin >> ptr[i];
+    }
+    for(int i=0; i<arraySize; i++){     //Array output
+        cout << ptr[i] << " ";
+    }
+    delete [] ptr;    //Deallocate memory
+}
