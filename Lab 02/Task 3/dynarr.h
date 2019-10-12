76f8edd4c3779a561f2f191bfe0629c137615351
Mehedi Hasan <mhasan502@gmail.com>
2019-10-12 12:33:42 +0545

    Create dynarr.h

---
+++ Lab 02/Task 3/dynarr.h
@@ -0,0 +1,20 @@
+#ifndef DYNARR_H
+#define DYNARR_H
+
+class dynarr
+{
+
+public:
+    dynarr();
+    dynarr(int, int);
+    ~dynarr();
+    void setValue(int, int, int);
+    int getValue(int, int);
+    void allocate(int, int);
+
+private:
+    int **data;
+    int row, col;
+};
+
+#endif // DYNARR_H
