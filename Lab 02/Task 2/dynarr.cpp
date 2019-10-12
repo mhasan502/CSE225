9a2016468602fddc0683baf8bc3c0a440edb60eb
Mehedi Hasan <mhasan502@gmail.com>
2019-10-12 12:16:23 +0545

    Create dynarr.cpp

---
+++ Lab 02/Task 2/dynarr.cpp
@@ -0,0 +1,30 @@
+#include "dynarr.h"
+#include<iostream>
+using namespace std;
+
+dynarr::dynarr()
+{
+    data = NULL;
+    size = 0;
+}
+
+dynarr::dynarr(int s)
+{
+    data = new int[s];
+    size = s;
+}
+dynarr::~dynarr()
+{
+    delete [] data;
+}
+int dynarr::getValue(int index)
+{
+    return data[index];
+}
+void dynarr::setValue(int index, int value)
+{
+    data[index] = value;
+}
+void dynarr::allocate(int s){
+    data = new int[s];
+}
