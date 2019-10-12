dfb629c7a74110a73f52b2c849d08a3d2434949d
Mehedi Hasan <mhasan502@gmail.com>
2019-10-12 12:35:03 +0545

    Create main.cpp

---
+++ Lab 02/Task 3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include"dynarr.h"
+using namespace std;
+
+int main()
+{
+    int row,col,input;
+    dynarr d1();
+    dynarr d2(5, 5);
+    cin >> row >> col;
+    d2.allocate(row, col);
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> input;
+            d2.setValue(i,j,input);
+        }
+    }
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout << d2.getValue(i,j) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
