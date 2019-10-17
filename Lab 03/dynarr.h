594503374709c20e28a21082b838df4468543677
Mehedi Hasan <mhasan502@gmail.com>
2019-10-17 12:37:03 +0545

    Create dynarr.h

---
+++ Lab 03/dynarr.h
@@ -0,0 +1,15 @@
+#ifndef DYNARR_H_INCLUDED
+#define DYNARR_H_INCLUDED
+
+template <class T>
+class dynArr{
+    private:
+        T *data;
+        int size;
+    public:
+        dynArr(int);
+        ~dynArr();
+        void setValue(int, T);
+        T getValue(int);
+};
+#endif // DYNARR_H_INCLUDED
