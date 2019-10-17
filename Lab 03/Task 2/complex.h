a6fb90cf6ca995ae0b96acafd2a92b8718515112
Mehedi Hasan <mhasan502@gmail.com>
2019-10-18 02:52:35 +0600

    Create complex.h

---
+++ Lab 03/Task 2/complex.h
@@ -0,0 +1,12 @@
+#ifndef COMPLEX_H_INCLUDED
+#define COMPLEX_H_INCLUDED
+class Complex {
+    public:
+        Complex();
+        Complex(double, double);
+        Complex operator+(Complex);
+        void Print();
+    private:
+        double Real, Imaginary;
+};
+#endif // COMPLEX_H_INCLUDED
