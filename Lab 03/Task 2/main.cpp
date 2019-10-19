5983eba023d5327870c037d1b53038d6ae0f4fb7
Mehedi Hasan <mhasan502@gmail.com>
2019-10-19 12:33:51 +0545

    Update main.cpp

--- Lab 03/Task 2/main.cpp
+++ Lab 03/Task 2/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "complex.h"
+#include "complex.cpp"
+using namespace std;
 
+int main(){
+
+    Complex c1(5,2), c2(5,3);
+    Complex c3 = c1+c2;
+    Complex c4 = c1*c2;
+    bool b = (c1!=c2);
+    c3.Print();
+    c4.Print();
+    cout << b << endl;
+
+    return 0;
+}
