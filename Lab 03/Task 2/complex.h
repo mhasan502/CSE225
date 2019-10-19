3e215113ffeefdc3b860b05f8f6b3550602e4f6d
Mehedi Hasan <mhasan502@gmail.com>
2019-10-19 12:33:06 +0545

    Update complex.h

--- Lab 03/Task 2/complex.h
+++ Lab 03/Task 2/complex.h
@@ -5,6 +5,8 @@ class Complex {
         Complex();
         Complex(double, double);
         Complex operator+(Complex);
+        Complex operator*(Complex);
+        bool operator!=(Complex);
         void Print();
     private:
         double Real, Imaginary;
