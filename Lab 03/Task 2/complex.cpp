b4bba0bf6fe7ae6b32587a11129fccbc9bc054e5
Mehedi Hasan <mhasan502@gmail.com>
2019-10-19 12:32:36 +0545

    Update complex.cpp

--- Lab 03/Task 2/complex.cpp
+++ Lab 03/Task 2/complex.cpp
@@ -16,6 +16,19 @@ Complex Complex::operator+(Complex a){
     t.Imaginary = Imaginary + a.Imaginary;
     return t;
 }
+Complex Complex::operator *(Complex a){
+    Complex t;
+    t.Real = Real * a.Real;
+    t.Imaginary = Imaginary * a.Imaginary;
+    return t;
+}
+bool Complex::operator !=(Complex a){
+    bool t = false;
+    if(Real==a.Real && Imaginary==a.Imaginary){
+       t = true;
+    }
+    return t;
+}
 void Complex::Print(){
-    cout << Real <<"+"<< Imaginary <<"i" << endl;
+    cout << Real <<" + "<< Imaginary <<"i" << endl;
 }
