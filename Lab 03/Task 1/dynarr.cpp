efdfb1c950b16d08a2ca76685e06de6ee5358556
Mehedi Hasan <mhasan502@gmail.com>
2019-10-25 20:25:28 +0600

    Update dynarr.cpp

--- Lab 03/Task 1/dynarr.cpp
+++ Lab 03/Task 1/dynarr.cpp
@@ -4,15 +4,12 @@ template<class T> dynArr<T>::dynArr(int s) {
     data = new T[s];
     size = s;
 }
-
 template<class T> dynArr<T>::~dynArr() {
     delete[] data;
 }
-
 template<class T> T dynArr<T>::getValue(int index) {
     return data[index];
 }
-
 template<class T> void dynArr<T>::setValue(int index, T value) {
     data[index] = value;
 }
