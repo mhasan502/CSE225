9686f9421ab644286c1dffdaa9cb223e8bd054d6
Mehedi Hasan <mhasan502@gmail.com>
2019-10-25 20:28:24 +0600

    Update dynarr.h

--- Lab 02/Task 2/dynarr.h
+++ Lab 02/Task 2/dynarr.h
@@ -1,20 +1,17 @@
 #ifndef DYNARR_H
 #define DYNARR_H
 
-class dynarr
-{
-
-public:
-    dynarr();
-    dynarr(int);
-    ~dynarr();
-    void setValue(int, int);
-    int getValue(int);
-    void allocate(int);
-
-private:
-    int *data;
-    int size;
+class dynarr{
+    public:
+        dynarr();
+        dynarr(int);
+        ~dynarr();
+        void setValue(int, int);
+        int getValue(int);
+        void allocate(int);
+    private:
+        int *data;
+        int size;
 };
 
 #endif // DYNARR_H
