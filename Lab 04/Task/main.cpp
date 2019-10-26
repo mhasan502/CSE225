844788cf24f32aa748da54b389b368519162528f
Mehedi Hasan <mhasan502@gmail.com>
2019-10-26 12:08:06 +0545

    Update main.cpp

--- Lab 04/Task/main.cpp
+++ Lab 04/Task/main.cpp
@@ -3,6 +3,19 @@
 #include "unsortedtype.cpp"
 using namespace std;
 
+void checkItem(bool b){
+    if(b)
+        cout << "Item is found" << endl;
+    else
+        cout << "Item is not found" << endl;
+}
+void checkFull(bool b){
+    if(b)
+        cout << "List is full" << endl;
+    else
+        cout << "List is not full" << endl;
+}
+
 int main(){
 
     UnsortedType<int> ut;
@@ -13,6 +26,27 @@ int main(){
     ut.Print();
     cout << ut.LengthIs() << endl;
     ut.InsertItem(1);
+    ut.Print();
+
+    int &value=4;
+    bool &boolVar;
+    ut.RetrieveItem(value,boolVar);
+    checkItem(boolVar);
+    value=5;
+    ut.RetrieveItem(value,boolVar);
+    checkItem(boolVar);
+    value=9;
+    ut.RetrieveItem(value,boolVar);
+    checkItem(boolVar);
+    value=10;
+    ut.RetrieveItem(value,boolVar);
+    checkItem(boolVar);
+    checkFull(ut.IsFull());
+    ut.DeleteItem(5);
+    checkFull(ut.IsFull());
+    ut.DeleteItem(1);
+    ut.Print();
+    ut.DeleteItem(6);
     ut.Print();
 
     return 0;
