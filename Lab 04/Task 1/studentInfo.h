30f7f50ea493b9c21c47bf955b0b479f96968a2f
Mehedi Hasan <mhasan502@gmail.com>
2019-10-26 12:35:50 +0545

    Create studentInfo.h

---
+++ Lab 04/Task 1/studentInfo.h
@@ -0,0 +1,13 @@
+#ifndef STUDENTINFO_H_INCLUDED
+#define STUDENTINFO_H_INCLUDED
+
+class studentInfo{
+    private:
+        int id;
+        string name;
+        double cgpa;
+    public:
+        studentInfo(int,string,double);
+};
+
+#endif // STUDENTINFO_H_INCLUDED
