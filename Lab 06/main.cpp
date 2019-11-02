e6b0d86ffa472cfcf35325d9d3522510be70d314
Mehedi Hasan <mhasan502@gmail.com>
2019-11-02 12:00:29 +0545

    Create main.cpp

---
+++ Lab 06/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "StackType.h"
+#include "StackType.cpp"
+using namespace std;
+
+void checkEmpty(bool b){
+    if(b)
+        cout << "Stack is Empty" <<endl;
+    else
+        cout << "Stack is not Empty" <<endl;
+}
+void checkFull(bool b){
+    if(b)
+        cout << "Stack is Full" <<endl;
+    else
+        cout << "Stack is not Full" <<endl;
+}
+void Print(StackType<int> st){
+    StackType<int> temp;
+
+    while(!st.IsEmpty()){
+        temp.Push(st.Pop());
+        st.Pop();
+    }
+    while(!temp.IsEmpty()){
+        st.Push(temp.Pop());
+        cout << temp.Top() << " ";
+        temp.Pop();
+    }
+    cout << endl;
+}
+
+int main(){
+
+    StackType<int> st;
+    checkEmpty(st.IsEmpty());
+    st.Push(5);
+    st.Push(7);
+    st.Push(4);
+    st.Push(2);
+    checkEmpty(st.IsEmpty());
+    checkFull(st.IsFull());
+
+    Print(st);
+    st.Push(3);
+    Print(st);
+
+    checkFull(st.IsFull());
+
+    st.Pop();
+    st.Pop();
+
+    cout << st.Top() << endl;
+
+    return 0;
+}
