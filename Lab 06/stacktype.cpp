e8568924221c26f54f4e8dfdac2c55a0a32d464d
Mehedi Hasan <mhasan502@gmail.com>
2019-11-07 23:57:50 +0600

    Update stacktype.cpp

--- Lab 06/stacktype.cpp
+++ Lab 06/stacktype.cpp
@@ -2,38 +2,32 @@
 using namespace std;
 
 template <class ItemType>
-StackType<ItemType>::StackType()
-{
+StackType<ItemType>::StackType(){
     top = -1;
 }
 template <class ItemType>
-bool StackType<ItemType>::IsEmpty()
-{
+bool StackType<ItemType>::IsEmpty(){
     return (top == -1);
 }
 template <class ItemType>
-bool StackType<ItemType>::IsFull()
-{
+bool StackType<ItemType>::IsFull(){
     return (top == MAX_ITEMS-1);
 }
 template <class ItemType>
-void StackType<ItemType>::Push(ItemType newItem)
-{
+void StackType<ItemType>::Push(ItemType newItem){
     if( IsFull() )
         throw FullStack();
     top++;
     items[top] = newItem;
 }
 template <class ItemType>
-void StackType<ItemType>::Pop()
-{
+void StackType<ItemType>::Pop(){
     if( IsEmpty() )
         throw EmptyStack();
     top--;
 }
 template <class ItemType>
-ItemType StackType<ItemType>::Top()
-{
+ItemType StackType<ItemType>::Top(){
     if (IsEmpty())
         throw EmptyStack();
     return items[top];
