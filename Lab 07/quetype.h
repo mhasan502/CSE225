fbf25b074aae2a4d38abff9df33a3229fc03aacf
Mehedi Hasan <mhasan502@gmail.com>
2019-11-06 21:01:22 -0800

    Create quetype.h

---
+++ Lab 07/quetype.h
@@ -0,0 +1,24 @@
+#ifndef QUETYPE_H_INCLUDED
+#define QUETYPE_H_INCLUDED
+
+class FullQueue {};
+class EmptyQueue {};
+
+template<class ItemType> 
+class QueType {
+  public:
+    QueType();
+    QueType(int max);
+    ~QueType();
+    void MakeEmpty();
+    bool IsEmpty();
+    bool IsFull();
+    void Enqueue(ItemType);
+    void Dequeue(ItemType&);
+  private:
+    int front;
+    int rear;
+    ItemType* items;
+    int maxQue;
+};
+#endif // QUETYPE_H_INCLUDED
