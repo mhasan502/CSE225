28c3f8de5c4e373e88f45f397b544ddf2c60b7fe
Mehedi Hasan <mhasan502@gmail.com>
2019-11-07 23:05:36 +0600

    Update studentInfo.h

--- Lab 04/Task 2/studentInfo.h
+++ Lab 04/Task 2/studentInfo.h
@@ -7,7 +7,13 @@ class studentInfo{
         string name;
         double cgpa;
     public:
+        studentInfo();
+        studentInfo(int);
         studentInfo(int,string,double);
+        bool operator ==(studentInfo s);
+        bool operator !=(studentInfo s);
+        friend ostream& operator << (ostream&, studentInfo&); // just like ToString method in Java
+
 };
 
 #endif // STUDENTINFO_H_INCLUDED
