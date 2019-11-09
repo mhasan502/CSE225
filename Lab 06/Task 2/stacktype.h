46dc6615d5ec4f5619c20170cb74e359385ea362
Mehedi Hasan <mhasan502@gmail.com>
2019-11-09 11:32:28 +0545

    Create stacktype.h

---
+++ Lab 06/Task 2/stacktype.h
@@ -0,0 +1,25 @@
+#ifndef STACKTYPE_H_INCLUDED
+#define STACKTYPE_H_INCLUDED
+const int MAX_ITEMS = 5;
+class FullStack
+// Exception class thrown
+// by Push when stack is full.
+{};
+class EmptyStack
+// Exception class thrown
+// by Pop and Top when stack is emtpy.
+{};
+template <class ItemType>
+class StackType {
+    public:
+        StackType();
+        bool IsFull();
+        bool IsEmpty();
+        void Push(ItemType);
+        void Pop();
+        ItemType Top();
+    private:
+        int top;
+        ItemType items[MAX_ITEMS];
+};
+#endif // STACKTYPE_H_INCLUDED
