9b84d95eab5fdf6e034f3c9aed9c8913d5fa1789
Mehedi Hasan <mhasan502@gmail.com>
2019-11-09 11:10:21 +0545

    Create stacktype.h

---
+++ stacktype.h
@@ -0,0 +1,24 @@
+#ifndef STACKTYPE_H_INCLUDED
+#define STACKTYPE_H_INCLUDED
+class FullStack
+{};
+class EmptyStack
+{};
+template <class ItemType>
+class StackType{
+    struct NodeType{
+        ItemType info;
+        NodeType* next;
+    };
+    public:
+        StackType();
+        ~StackType();
+        void Push(ItemType);
+        void Pop();
+        ItemType Top();
+        bool IsEmpty();
+        bool IsFull();
+    private:
+        NodeType* topPtr;
+};
+#endif // STACKTYPE_H_INCLUDED
