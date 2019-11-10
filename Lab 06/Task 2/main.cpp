7092bf6af761dcac6c20cb014784e6e5e5cf54f5
Mehedi Hasan <mhasan502@gmail.com>
2019-11-10 10:50:17 +0600

    Update main.cpp

--- Lab 06/Task 2/main.cpp
+++ Lab 06/Task 2/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(){
 
     StackType<char> st;
-    string str = ")(";
-    int count = 0;
+    string str;
+    cin >> str;
 
     for(int i=0; str[i]!='\0'; i++){
         if(str[i]=='('){
