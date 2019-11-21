ead58ef728dae9d72300fa021af5e5b49e2b5d06
Mehedi Hasan <mhasan502@gmail.com>
2019-11-21 12:41:33 +0545

    Update main.cpp

--- Lab 08/main.cpp
+++ Lab 08/main.cpp
@@ -3,24 +3,43 @@
 #include <string>
 using namespace std;
 
+bool isOperator(char ch){
+    if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='('||ch==')')
+        return true;
+    else
+        return false;
+}
 bool priority(char ch){
     if(ch=='+') return 1;
     else if(ch=='-') return 2;
     else if(ch=='*') return 3;
     else if(ch=='/') return 4;
+    else if(ch=='(') return 5;
+    else if(ch==')') return 0;
 
 
 }
 
 int main(){
     string input, postfix;
-    cin >> input;
+    input = "(2+3)*(4+5)*8";
     StackType<char> st;
     for(int i=0; i<input.size(); i++){
-        if(isdigit(input[i])){
+        if(input[i]=='('||input[i]==')'){
+            if(input[i]=='(')
+                st.Push(input[i]);
+            else{
+                while(!st.IsEmpty()){
+                    postfix+=st.Top();
+                    st.Pop();
+                }
+            }
+        }
+
+        else if(isdigit(input[i])){
             postfix+=input[i];
         }
-        else{
+        else if(isOperator(input[i])){
             if(st.IsEmpty())
                 st.Push(input[i]);
             else if(priority(st.Top())>=priority(input[i]))
